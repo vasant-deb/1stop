61e8bf93c7c221fe3177aa5d33364a6e2fd7ec64
vasant-deb <iamvasantsharma@gmail.com>
2023-03-13 23:34:38 -0600

    update1.0

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -6,89 +6,92 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class AuthService {
+  
   constructor(private http: HttpClient) {}
+  
+ 
 
   signup(value: {firstName: string,middleName: string,lastName: string,referredBy: string,docType: string,addressName: string,company: string,address: string,address2: string,city: string,zipcode: string,state: string,phone: number,gst: string,email:string,password: string,confirmPassword: string}) {
-    return this.http.post<{message:string,email:string,token: string}>('https://checklistforme.online/api/public/register', value);
+    return this.http.post<{message:string,email:string,token: string}>('https://api.yusyah.com/public/register', value);
   }
 
   login(value: {email: string, password: string }) {
-    return this.http.post<{error:boolean,message:string,email:string,token: string}>('https://checklistforme.online/api/public/login', value);
+    return this.http.post<{error:boolean,message:string,email:string,token: string}>('https://api.yusyah.com/public/login', value);
   }
   checkauths({ email, token }: { email: string, token: string }): Observable<any> {
-    return this.http.post<any>('https://checklistforme.online/api/public/checkauth', { email, token });
+    return this.http.post<any>('https://api.yusyah.com/public/checkauth', { email, token });
   }
   updateaddress({updatedAddress}: { updatedAddress:any }): Observable<any> {
-    return this.http.post<any>('https://checklistforme.online/api/public/updateaddress', {updatedAddress });
+    return this.http.post<any>('https://api.yusyah.com/public/updateaddress', {updatedAddress });
   }
   updatepassword({change}: { change:any }): Observable<any> {
-    return this.http.post<any>('https://checklistforme.online/api/public/updatepassword', {change });
+    return this.http.post<any>('https://api.yusyah.com/public/updatepassword', {change });
   }
   getorderhistory({ email, token }: { email: string, token: string }): Observable<{ error: boolean ,stats:any[]}> {
-    return this.http.post<{ error: boolean,stats:any[] }>('https://checklistforme.online/api/public/orderhistory', { email, token });
+    return this.http.post<{ error: boolean,stats:any[] }>('https://api.yusyah.com/public/orderhistory', { email, token });
   }
   checkconfirm({shippingId, billingId  }: { shippingId: string, billingId: string }): Observable<{ error: boolean ,stats:any[]}> {
-    return this.http.post<{ error: boolean ,stats:any[]}>('https://checklistforme.online/api/public/confirmdetails', { shippingId, billingId });
+    return this.http.post<{ error: boolean ,stats:any[]}>('https://api.yusyah.com/public/confirmdetails', { shippingId, billingId });
   }
   checkout({shippingId, billingId,token,email }: { shippingId: string, billingId: string,email: string, token: string }): Observable<{ error: boolean,message:string}> {
-    return this.http.post<{ error: boolean,message:string }>('https://checklistforme.online/api/public/checkout', { shippingId, billingId ,token,email});
+    return this.http.post<{ error: boolean,message:string }>('https://api.yusyah.com/public/checkout', { shippingId, billingId ,token,email});
   }
   deletecart({ cartId }: { cartId: number }): Observable<{ error: boolean }> {
-    return this.http.post<{ error: boolean }>('https://checklistforme.online/api/public/deletecartitem', { cartId });
+    return this.http.post<{ error: boolean }>('https://api.yusyah.com/public/deletecartitem', { cartId });
   }
   userdetails(value: {email: string, token: string }) {
-    return this.http.post<{message:string,firstname:string,lastname: string,email: string,countcart:number,total:number,cart: any[]}>('https://checklistforme.online/api/public/userdetails', value);
+    return this.http.post<{message:string,firstname:string,lastname: string,email: string,countcart:number,total:number,cart: any[]}>('https://api.yusyah.com/public/userdetails', value);
   }
   getcategories() {
-    return this.http.get<{message:string,stats: any[]}>('https://checklistforme.online/api/public/categories');
+    return this.http.get<{message:string,stats: any[]}>('https://api.yusyah.com/public/categories');
   }
   addTocart(cartItems: any[], token: string | null, email: string | null): Observable<any> {
     let params = new HttpParams().set('token', token || '').set('email', email || '');
-    return this.http.post<any>(`https://checklistforme.online/api/public/addalltoacart`, { cartItems }, { params });
+    return this.http.post<any>(`https://api.yusyah.com/public/addalltoacart`, { cartItems }, { params });
 }
   getCategory(slug: string | null, token: string | null, email: string | null): Observable<any> {
     let params = new HttpParams().set('token', token || '').set('email', email || '');
-    return this.http.get<any>(`https://checklistforme.online/api/public/getcategory/${slug}`, { params });
+    return this.http.get<any>(`https://api.yusyah.com/public/getcategory/${slug}`, { params });
   }
   getPage(slug: string | null, token: string | null, email: string | null): Observable<any> {
     let params = new HttpParams().set('token', token || '').set('email', email || '');
-    return this.http.get<any>(`https://checklistforme.online/api/public/getpage/${slug}`, { params });
+    return this.http.get<any>(`https://api.yusyah.com/public/getpage/${slug}`, { params });
   }
   getOrderDetails(id: string | null): Observable<any> {
-    return this.http.post<any>(`https://checklistforme.online/api/public/getorderdetails`, { id });
+    return this.http.post<any>(`https://api.yusyah.com/public/getorderdetails`, { id });
   }
   getCartItems(token: string | null, email: string | null): Observable<any> {
     let params = new HttpParams().set('token', token || '').set('email', email || '');
-    return this.http.get<any>(`https://checklistforme.online/api/public/getcartitems`, { params });
+    return this.http.get<any>(`https://api.yusyah.com/public/getcartitems`, { params });
   }
   
   getAddress(token: string | null, email: string | null): Observable<any> {
     let params = new HttpParams().set('token', token || '').set('email', email || '');
-    return this.http.get<any>(`https://checklistforme.online/api/public/getAddress`, { params });
+    return this.http.get<any>(`https://api.yusyah.com/public/getAddress`, { params });
   }
   getProduct(slug: string | null, token: string | null): Observable<any> {
     let params = new HttpParams().set('token', token || '');
-    return this.http.get<any>(`https://checklistforme.online/api/public/getproduct/${slug}`, { params });
+    return this.http.get<any>(`https://api.yusyah.com/public/getproduct/${slug}`, { params });
   }
   getaccessories(token: string | null, email: string | null): Observable<any> {
     let params = new HttpParams().set('token', token || '').set('email', email || '');
-    return this.http.get<any>(`https://checklistforme.online/api/public/accessories`, { params });
+    return this.http.get<any>(`https://api.yusyah.com/public/accessories`, { params });
   }
   getslides() {
-    return this.http.get<{message:string,stats: any[]}>('https://checklistforme.online/api/public/slides');
+    return this.http.get<{message:string,stats: any[]}>('https://api.yusyah.com/public/slides');
   }
   getlatestdevices(){
-    return this.http.get<{message:string,stats: any[]}>('https://checklistforme.online/api/public/latestdevices');
+    return this.http.get<{message:string,stats: any[]}>('https://api.yusyah.com/public/latestdevices');
   }
   getdevices(){
-    return this.http.get<{message:string,stats: any[]}>('https://checklistforme.online/api/public/devices');
+    return this.http.get<{message:string,stats: any[]}>('https://api.yusyah.com/public/devices');
   }
   getmenus(){
-    return this.http.get<{message:string,stats: any[]}>('https://checklistforme.online/api/public/menus');
+    return this.http.get<{message:string,stats: any[]}>('https://api.yusyah.com/public/menus');
   }
  
   
   saleproducts(){
-    return this.http.get<{message:string,stats: any[]}>('https://checklistforme.online/api/public/saleproducts');
+    return this.http.get<{message:string,stats: any[]}>('https://api.yusyah.com/public/saleproducts');
   }
 }
